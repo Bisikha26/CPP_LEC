257b108297edeb1ca3723f655e43ed7d2d501f2b
Bisikha <bisikhasubedi26@gmail.com>
2021-06-11 11:30:55 +0545

    const, array

---
+++ Class_Content/chapter3_intro/Misc/array.cpp
@@ -0,0 +1,23 @@
+//Array declaration by initializing elements
+//int arr[] = { 10, 20, 30, 40 };
+ 
+//Compiler creates an array of size 4 which is same as int arr[4]={10, 20, 30, 40}
+
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int arr[5];
+    arr[0] = 5;
+    arr[2] = -10;
+ 
+    // this is same as arr[1] = 2
+    arr[3 / 2] = 2;
+    arr[3] = arr[0];
+ 
+    cout << arr[0] << " " << arr[1] << " " << arr[2] << " "
+         << arr[3];
+ 
+    return 0;
+}
---
+++ Class_Content/chapter3_intro/Misc/const.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+  
+#define val 10 
+#define floatVal 4.5 
+#define charVal 'G' 
+  
+int main() {
+    cout << "Integer Constant: " << val << "\n"; 
+    cout << "Floating point Constant: " << floatVal << "\n"; 
+    cout << "Character Constant: "<< charVal << "\n"; 
+      
+    return 0; 
+}
